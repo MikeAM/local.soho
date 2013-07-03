a7f73e5ca27ac4efe9bcb6d29df9612cd5f5fb5c
Mike <mike.morrison@soholaunch.com>
2013-07-03 18:55:22 -0400

    Fix most paths in template. Page content as content files in media.

--- sohoadmin/program/modules/site_templates/pages/custom-soholaunch-2013/index.php
+++ sohoadmin/program/modules/site_templates/pages/custom-soholaunch-2013/index.php
@@ -1,39 +1,152 @@
-<?php
-error_reporting(E_PARSE);
-function cleanInput($input) {
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js	lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js	lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js	lt-ie9">    <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Soholaunch Website Builder</title>
+    <meta name="description" content="Soholaunch Website builder helps you create better websites, faster.">
+    <link href='http://fonts.googleapis.com/css?family=Marck+Script' rel='stylesheet' type='text/css'>
+    <meta name="viewport" content="width=device-width">
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <link rel="stylesheet" href="/sohoadmin/program/modules/site_templates/pages/custom-soholaunch-2013/css/main.css">
+    <!-- Fonts -->
+    <script type="text/javascript" src="//use.typekit.net/gcx2oko.js"></script>
+    <script type="text/javascript">try {
+            Typekit.load();
+        } catch (e) {
+        }</script>
+</head>
+<body class="home">
+<!--[if lt IE 7]>
+<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to
+    improve your experience.</p>
+<![endif]-->
 
-    $search = array(
-        '@<script[^>]*?>.*?</script>@si',   // Strip out javascript
-        '@<[\/\!]*?[^<>]*?>@si',            // Strip out HTML tags
-        '@<style[^>]*?>.*?</style>@siU',    // Strip style tags properly
-        '@<![\s\S]*?--[ \t\n\r]*>@'         // Strip multi-line comments
-    );
+<div class="container-outer">
+    <header class="container body">
+        <div class="grid-row">
+            <div class="columns five">
+                <h1><a href="/"><img src="img/soholaunch-logo.png" id="soholaunch-logo" height="96" data-fixedwh="true"/></a></h1>
+            </div>
 
-    $output = preg_replace($search, '', $input);
-    return $output;
-}
-function sanitize($input) {
-    if (is_array($input)) {
-        foreach($input as $var=>$val) {
-            $output[$var] = sanitize($val);
-        }
-    }
-    else {
-        if (get_magic_quotes_gpc()) {
-            $input = stripslashes($input);
-        }
-        $input  = cleanInput($input);
-        $output = mysql_real_escape_string($input);
-    }
-    return $output;
-}
+            <div class="columns seven">
+                <nav role="primary">
+                    <a href="home">Home</a>
+                    <a href="features">Features</a>
+                    <!--<a href="/developers">Developers</a>-->
+                    <a href="support">Support</a>
+                    <a href="buy" class="buy">Buy</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    #content#
+
+    <!--- FOOTER starts here--->
+    <footer class="container dark">
+        <div class="grid-row">
+            <div class="columns four">
+                <h2>Latest News</h2>
+
+                <p>YESTERDAY Ultra v1.34 Update Released</p>
+                <p>MAR 29 2013 Ultra v1.33 Update Released</p>
+                <p></p>MAR 15 2013 Free Code: Pure CSS basline.</p>
+
+                <p>Subscribe - You'll get an email when new versions
+                    become available, including a list of changes.</p>
+            </div>
+
+            <div class="columns four">
+                <h2>Helpful Links</h2>
+
+                <a href="http://partner.soholaunch.com"
+                   title="Soholaunch Partner Area: Submit support tickets, manager licenses.">
+                    <h3>Soholaunch Customer Area</h3>
+
+                    <p>Manage your licenses, submit support tickets,
+                        update account settings.</p>
+                </a>
+
+                <a href="http://partner.soholaunch.com"
+                   title="Soholaunch Partner Area: Submit support tickets, manager licenses.">
+                    <h3>Soholaunch Addons</h3>
+
+                    <p>Download and/or purchase additional templates and add-on features for Soholaunch</p>
+                </a>
+
+                <a href="http://partner.soholaunch.com"
+                   title="Soholaunch Partner Area: Submit support tickets, manager licenses.">
+                    <h3>Soholaunch Wiki Documentation</h3>
+
+                    <p>Learn how to create templates and plugins,
+                        get answers to common support questions.</p>
+                </a>
+            </div>
+
+            <div class="columns four">
+                <h2>Soholaunch.com, Inc.</h2>
+                <ul>
+                    <li>sales@soholauch.com</li>
+                    <li>@Soholaunch</li>
+                    <li>facebook.com/soholaunch</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</div>
+<!-- END .container -->
 
 
-$page = sanitize($_GET['pr']);
-if ( $page == '' ) { $page = 'home'; }
+<!-- START Behavior Layer -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/sohoadmin/program/modules/site_templates/pages/custom-soholaunch-2013/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+<script src="/sohoadmin/program/modules/site_templates/pages/custom-soholaunch-2013/js/plugins.js"></script>
+<script src="/sohoadmin/program/modules/site_templates/pages/custom-soholaunch-2013/js/main.js"></script>
 
-$template_html = file_get_contents('_template.html');
-$page_html = file_get_contents($page.'.html');
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    var _gaq = [
+        ['_setAccount', 'UA-XXXXX-X'],
+        ['_trackPageview']
+    ];
+    (function (d, t) {
+        var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        g.src = '//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g, s)
+    }(document, 'script'));
+</script>
 
-echo str_replace('#content#', $page_html, $template_html);
-?>
+
+<!--Custom js for soholaunch.com-->
+<script src="/sohoadmin/program/modules/site_templates/pages/custom-soholaunch-2013/js/buy-pages.js"></script>
+
+
+<!-- Grid overlay -->
+<link href="headsupgrid/hugrid.css" type="text/css" rel="stylesheet"/>
+<script src="headsupgrid/hugrid.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        pageUnits = 'px';
+        colUnits = 'px';
+        pagewidth = 1000;
+        columns = 12;
+        columnwidth = 65;
+        gutterwidth = 20;
+        pagetopmargin = 0;
+        rowheight = 12;
+        gridonload = 'off';
+        makehugrid();
+        setgridonload();
+    });
+</script>
+</body>
+</html>
