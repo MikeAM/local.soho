ab3c64828550a538a0bc5979d62bbaae746f14c5
Mike <mike.morrison@soholaunch.com>
2013-07-04 17:41:42 -0400

    Try to ignore sohoadmin

--- sohoadmin/program/modules/tiny_mce/plugins/Addfontz/langs/en.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-//tinyMCE.addToLang('Addfontz',{
-//title : 'Font Manager',
-//desc : 'Font Manager'
-//});
--- sohoadmin/program/modules/tiny_mce/plugins/Uploadfile/langs/en.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-//tinyMCE.addToLang('Uploadfile',{
-//title : 'Upload Files',
-//desc : 'Upload Files'
-//});
